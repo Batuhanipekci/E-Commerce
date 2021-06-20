e7c81dcb579e1bf07a7a501a6f732eea1097f9e1
Batuhan Ipekci <batuhanipekci@hotmail.com>
2021-06-20 22:11:23 +0200

    Models for the Analytics module

---
+++ api/analytics/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class KrDetailsView(models.Model):
+    user = models.ForeignKey("KrUser", on_delete=models.CASCADE, db_column="kr_user_uuid")
+    event = models.ForeignKey("KrEvent", on_delete=models.CASCADE, db_column="kr_event_id")
+    article = models.ForeignKey("KrArticle", on_delete=models.CASCADE, db_column="kr_article_id")
+    ts = models.DateTimeField()
+
+    class Meta:
+        db_table = "kr_details_view"
+
+
+class KrTransaction(models.Model):
+    user = models.ForeignKey("KrUser", on_delete=models.CASCADE, db_column="kr_user_uuid")
+    event = models.ForeignKey("KrEvent", on_delete=models.CASCADE, db_column="kr_event_id")
+    article = models.ForeignKey("KrArticle", on_delete=models.CASCADE, db_column="kr_article_id")
+    order = models.ForeignKey("KrOrder", on_delete=models.CASCADE, db_column="kr_order_id", blank=True, null=True)
+    ts = models.DateTimeField()
+
+    class Meta:
+        db_table = "kr_transaction"
+
+
+# Connect to transaction signal
+class KrCounter(models.Model):
+    user = models.ForeignKey("KrUser", on_delete=models.CASCADE, db_column="kr_user_uuid")
+    article = models.ForeignKey("KrArticle", on_delete=models.CASCADE, db_column="kr_article_id")
+    event = models.ForeignKey("KrEvent", on_delete=models.CASCADE, db_column="kr_event_id")
+    count = models.IntegerField(null=False, blank=False, default=0)
+    ts_begin = models.DateTimeField()
+    ts_end = models.DateTimeField()
+
+    class Meta:
+        db_table = 'kr_counter'
+    
+    def __str__(self):
+        return f"{self.user.email}-{self.article.name}-{self.event.name}"
+
+
+class KrSummary(models.Model):
+    article = models.ForeignKey("KrArticle", on_delete=models.CASCADE, db_column="kr_article_id")
+    detail_view_count = models.IntegerField()
+    transaction_count = models.IntegerField()
+    total_sales_revenue = models.IntegerField()
+    ts_begin = models.DateTimeField()
+    ts_end = models.DateTimeField()
+
+    class Meta:
+        db_table = "kr_summary"
+
