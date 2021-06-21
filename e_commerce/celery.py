a312fafc1fb59eef6404036ab8767660e1ce33f2
Batuhan Ipekci <batuhanipekci@hotmail.com>
2021-06-21 02:07:54 +0200

    Configure Celery to find out the tasks

--- e_commerce/celery.py
+++ e_commerce/celery.py
@@ -6,5 +6,8 @@ from django.conf import settings
 # Set the default Django settings module for the celery program
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_commerce.settings')
 
-app = Celery('e-commerce')
+app = Celery('e_commerce')
 
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
